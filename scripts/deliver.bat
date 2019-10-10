131ed7353ad337cbb33a67baff0e9f5d3d77245c
MAurelie <martinez.aurelie.cf@gmail.com>
2019-10-10 16:21:32 +0200

    add finalName

--- scripts/deliver.bat
+++ scripts/deliver.bat
@@ -1,5 +1,4 @@
 @echo off
-set ARTIFACT=mc.cartes-0.0.1-SNAPSHOT.jar
-set NEWNAME=myjavacarte.jar
+set ARTIFACT=superPoker.jar
 set DESTDIR=C:\Users\User\Jenkins\DepotPackages
-copy target\%ARTIFACT% %DESTDIR%\\%NEWNAME% 
+copy target\%ARTIFACT% %DESTDIR%
