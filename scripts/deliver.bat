ad0da76d2e7939228180307c85727d9edf022502
MAurelie <martinez.aurelie.cf@gmail.com>
2019-10-10 15:27:55 +0200

    new name for script

--- scripts/deliver.bat
+++ scripts/deliver.bat
@@ -1,4 +1,5 @@
 @echo off
 set ARTIFACT=mc.cartes-0.0.1-SNAPSHOT.jar
+set NEWNAME=myjavacarte.jar
 set DESTDIR=C:\Users\User\Jenkins\DepotPackages
-copy target\%ARTIFACT% %DESTDIR%
+copy target\%ARTIFACT% %DESTDIR%\\%NEWNAME% 
